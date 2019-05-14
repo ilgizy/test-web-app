1b7dd8d367457c1b60d47fdc5334aa14e3c853fd
Ильгиз Юсупов <iyu@ya.ru>
2019-05-14 11:13:07 +0500

    first commit

---
+++ TestWebApp/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace TestWebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("test-api", new Info
+                {
+                    Description = "Описание методов",
+                    TermsOfService = "",
+                    Version = "test-api"
+                });
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.RoutePrefix = "api-docs";
+                    c.SwaggerEndpoint("/swagger/test-api/swagger.json", "test-api");
+                }
+                );
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+docker build -t test-web-app -f .\TestWebApp\Dockerfile .
+
+docker run -d -p 7000:80 test-web-app
